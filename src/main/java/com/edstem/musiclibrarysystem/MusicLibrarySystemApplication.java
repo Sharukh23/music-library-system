42a134f0cf71ae0cbe994bfa61ff81efc1c47ef5
Sharok N <Sharukhahamed23@gmail.com>
2023-10-29 12:34:42 +0530

    first commit

---
+++ src/main/java/com/edstem/musiclibrarysystem/MusicLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.edstem.musiclibrarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLibrarySystemApplication.class, args);
+	}
+
+}
